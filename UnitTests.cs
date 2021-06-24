cc98629d31379ce7314cb0efd99a221ebdcdb771
Olivier DALET <olivier.dalet@addupsolutions.com>
2021-06-24 16:47:27 +0200

    Playing with AutoResetEvent

--- UnitTests.cs
+++ UnitTests.cs
@@ -10,34 +10,34 @@ namespace MSTestTest
     [TestClass, ExcludeFromCodeCoverage]
     public class UnitTests
     {
-        // This test works when using MSTest 2.1, but not in MSTest 2.2 in a Docker
-        [TestMethod, ExpectedException(typeof(BrokerUnreachableException))]
-        public void RabbitClient_cannot_connect_when_passed_invalid_host_and_credentials()
-        {
-            var factory = new ConnectionFactory
-            {
-                HostName = "IDONTEXIST",
-                UserName = "jdoe",
-                Password = "p@ssw0rd",
-                Port = 5672,
-                AutomaticRecoveryEnabled = true,
-                TopologyRecoveryEnabled = true
-            };
+        //// This test works when using MSTest 2.1, but not in MSTest 2.2 in a Docker
+        //[TestMethod, ExpectedException(typeof(BrokerUnreachableException))]
+        //public void RabbitClient_cannot_connect_when_passed_invalid_host_and_credentials()
+        //{
+        //    var factory = new ConnectionFactory
+        //    {
+        //        HostName = "IDONTEXIST",
+        //        UserName = "jdoe",
+        //        Password = "p@ssw0rd",
+        //        Port = 5672,
+        //        AutomaticRecoveryEnabled = true,
+        //        TopologyRecoveryEnabled = true
+        //    };
 
-            try
-            {
-                _ = factory.CreateConnection("ncore");
-                Assert.Fail("Connection should be impossible");
-            }
-            catch (BrokerUnreachableException)
-            {                
-                throw; // That's what we expect
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail($"Connection creation error: {ex}");
-            }
-        }
+        //    try
+        //    {
+        //        _ = factory.CreateConnection("ncore");
+        //        Assert.Fail("Connection should be impossible");
+        //    }
+        //    catch (BrokerUnreachableException)
+        //    {                
+        //        throw; // That's what we expect
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Assert.Fail($"Connection creation error: {ex}");
+        //    }
+        //}
 
         [TestMethod]
         public void TryConnectAsync_is_cancelable()
@@ -67,6 +67,27 @@ namespace MSTestTest
             catch (Exception ex)
             {
                 // This happens when running in GitLab CI...
+                Assert.Fail($"Test failed: {ex}");
+            }
+        }
+
+        [TestMethod]
+        public void Repro()
+        {
+            try
+            {
+                using var done = new AutoResetEvent(false);
+                var thread = new Thread(() =>
+                {
+                    Thread.Sleep(2000);
+                    _ = done.Set();
+                });
+
+                thread.Start();
+                //_ = done.WaitOne(1000); // Wait for the thread to finish (and timeout after 1s)
+            }
+            catch (Exception ex)
+            {
                 Assert.Fail($"Test failed: {ex}");
             }
         }
